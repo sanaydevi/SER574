be9909e965c4c476ced526a74d54749a96c3269b
Subhradeep Biswas <32438829+subhradeepbiswas@users.noreply.github.com>
2019-02-04 11:58:54 -0700

    Final version of Data Generator and GUI

--- Core/DataGenerator.java
+++ Core/DataGenerator.java
@@ -1,4 +1,7 @@
+package Core;
+
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Random;
 import java.util.Calendar;
@@ -6,8 +9,24 @@ import java.util.Calendar;
 class DataGenerator extends Observable implements Runnable {
 
     private Data data;
+    private Gui val;
     private boolean stop = false;
-    private int input_face=0;
+    //private int input_face=0;
+    
+    
+    protected static HashMap<String, Double> faceParameters = new HashMap<String, Double>() 
+    {
+  	  {
+	  	put("agreement", (double) 0.0);
+	  	put("disagreement", (double) 0);
+	  	put("frustrate", (double) 0);
+	  	put("concentrate", (double) 0);
+	  	put("thinking", (double) 0);
+	  	put("unsure", (double) 0);
+	  	
+	  }
+  	};
+
 
     public void stop() {
         this.stop = true;
@@ -28,56 +47,51 @@ class DataGenerator extends Observable implements Runnable {
 
         while (!stop) {
             System.out.println("data generator running");
+            float agreeInput = (float) (faceParameters.get("agreement")/10);
+            float disagreeInput = (float) (faceParameters.get("disagreement")/10);
+            float frusInput = (float) (faceParameters.get("frustrate")/10);
+            float concenInput = (float) (faceParameters.get("concentrate")/10);
+            float thinkInput = (float) (faceParameters.get("thinking")/10);
+            float unsureInput = (float) (faceParameters.get("unsure")/10);
             timeStamp = (System.currentTimeMillis() - initialTime) * .001;
-            float max = (float) ((Math.random()*((1-0.7)))+0.7);
-            float min1 =  (float) ((Math.random()*((0.6-0.01)))+0.01);
-            float min2 =  (float) ((Math.random()*((0.6-0.01)))+0.01);
-            float min3 =  (float) ((Math.random()*((0.6-0.01)))+0.01);
-            float min4 =  (float) ((Math.random()*((0.6-0.01)))+0.01);
-            float min5 =  (float) ((Math.random()*((0.6-0.01)))+0.01);
-            if(input_face == 1)
-                createAndNotify(timeStamp,max,
-                        min1,min2,min3,min4,min5);
-            else if(input_face == 2)
-                createAndNotify(timeStamp,
-                        min1,max,min2,min3,min4,min5);
-            else if(input_face == 3)
-                createAndNotify(timeStamp,
-                        min1,min2,max,min3,min4,min5);
-            else if(input_face == 4)
-                createAndNotify(timeStamp,
-                        min1,min2,min3,max,min4,min5);
-            else if(input_face == 5)
-                createAndNotify(timeStamp,
-                        min1,min2,min3,min4,max,min5);
-            else if(input_face == 6)
-                createAndNotify(timeStamp,
-                        min1,min2,min3,min4,min5,max);
-            else
-                createAndNotify(timeStamp,
-                        min1,min2,min3,min4,min5,max);
+            float agree =  (float) ((agreeInput - 0.1) + new Random().nextFloat()*(agreeInput - (agreeInput - 0.1)));
+            float disagree =  (float) ((disagreeInput - 0.1) + new Random().nextFloat()*(disagreeInput - (disagreeInput - 0.1)));
+            float frustrate =  (float) ((frusInput - 0.1) + new Random().nextFloat()*(frusInput - (frusInput - 0.1)));
+            float concentrate =  (float) ((concenInput - 0.1) + new Random().nextFloat()*(concenInput - (concenInput - 0.1)));
+            float thinking =  (float) ((thinkInput - 0.1) + new Random().nextFloat()*(thinkInput - (thinkInput - 0.1)));
+            float unsure =  (float) ((unsureInput - 0.1) + new Random().nextFloat()*(unsureInput - (unsureInput - 0.1)));
+            
+            createAndNotify(timeStamp, agree, disagree, frustrate, concentrate, thinking, unsure);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
             }
+            
         }
     }
     public void run_data_generator(int value)
     {
-        input_face= value;
+        //input_face= value;
         run();
     }
+    
 
-    private void createAndNotify(double timestampsystem, float agreement, float concentrating, float disagreement, float interested, float thinking, float unsure) {
-        System.out.println("notifying ...");
-
-        data = new Data(timestampsystem, agreement, concentrating,disagreement,interested,thinking, unsure);
-        data.setData( timestampsystem, agreement, concentrating,disagreement,interested,thinking, unsure);
-        System.out.println(data.toString());
-        //setChanged();
-        //notifyObservers();
+    public void updateParam(String s, Double val) 
+    {
+  	  
+  	  faceParameters.put(s, Math.floor(val));
+  	  System.out.println(faceParameters);
+  	  
     }
 
+    private void createAndNotify(double timestampsystem, float agreement, float disagreement, float frustrate, float concentrating, float thinking, float unsure) {
+        System.out.println("notifying ...");
 
-
+        data = new Data(timestampsystem, agreement, concentrating, disagreement, frustrate, thinking, unsure);
+        data.setData( timestampsystem, agreement, concentrating, disagreement,frustrate, thinking, unsure);
+        
+        System.out.println(data.toString());
+        setChanged();
+        notifyObservers();
+    }
 }
--- Core/Gui.java
+++ Core/Gui.java
@@ -4,13 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Gui extends JPanel implements ActionListener {
 
@@ -19,10 +24,19 @@ public class Gui extends JPanel implements ActionListener {
   private final int PORT = 1594;
   protected JLabel labelPublishPort;
   private final JButton buttonConnect = new JButton("run");
+  private JPanel sliderPanel;
+  //private JPanel labels;
+  //private JPanel panel;
+  JSlider agreement = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
+  JSlider disagreement = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
+  JSlider frustrate = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
+  JSlider concentrate = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
+  JSlider thinking = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
+  JSlider unsure = new JSlider(JSlider.HORIZONTAL, 0, 10, 0);
 
-  private Component createPanelSouth() {
+  private Component createPanelSouth() {	
 
-    JPanel labels = new JPanel();
+	JPanel labels = new JPanel();
     labels.setBackground(Color.GRAY);
     labels.add(new JLabel("  Publishing at port: "));
     labelPublishPort = new JLabel("" + PORT);
@@ -35,18 +49,70 @@ public class Gui extends JPanel implements ActionListener {
     buttonConnect.setEnabled(true);
     return panel;
   }
+  
+  private Component createSlider() {
+	  
+	sliderPanel = new JPanel();
+	sliderPanel.setLayout(new GridLayout(6,1));
+	sliderPanel.setMinimumSize(new Dimension(600, 300));
+    //sliderPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    agreement.setMajorTickSpacing(10);
+	agreement.setMinorTickSpacing(1);
+	agreement.setPaintTicks(true);
+	agreement.setPaintLabels(true);
+	
+	disagreement.setMajorTickSpacing(10);
+	disagreement.setMinorTickSpacing(1);
+	disagreement.setPaintTicks(true);
+	disagreement.setPaintLabels(true);
+	
+	frustrate.setMajorTickSpacing(10);
+	frustrate.setMinorTickSpacing(1);
+	frustrate.setPaintTicks(true);
+	frustrate.setPaintLabels(true);
+	
+	concentrate.setMajorTickSpacing(10);
+	concentrate.setMinorTickSpacing(1);
+	concentrate.setPaintTicks(true);
+	concentrate.setPaintLabels(true);
+	
+	thinking.setMajorTickSpacing(10);
+	thinking.setMinorTickSpacing(1);
+	thinking.setPaintTicks(true);
+	thinking.setPaintLabels(true);
+	
+	unsure.setMajorTickSpacing(10);
+	unsure.setMinorTickSpacing(1);
+	unsure.setPaintTicks(true);
+	unsure.setPaintLabels(true);
+	
+
+	addSlider(agreement, "Agreement", "agreement");
+	addSlider(disagreement, "Disagreement", "disagreement");
+	addSlider(frustrate, "Frustrate", "frustrate");
+	addSlider(concentrate, "Concentrate", "concentrate");
+	addSlider(thinking, "Thinking", "thinking");
+	addSlider(unsure, "Unsure", "unsure");
+	
+	return sliderPanel;
+	  
+  }
 
   public Gui() {
 
     model = new Model(new DataGenerator(), new Publisher(PORT));
     this.setBackground(Color.WHITE);
     this.setLayout(new BorderLayout());
+    //this.setLayout(new GridLayout(3,2));
     this.add(createPanelSouth(), BorderLayout.SOUTH);
+    this.add(createSlider(), BorderLayout.CENTER);
+	
     Dimension screen = getToolkit().getScreenSize();
-    this.setSize(screen.width / 2, 3 * screen.height / 4);
+    this.setSize(screen.width / 2, 3 * (screen.height / 4));
     this.setLocation((screen.width - getSize().width) / 2, (screen.height - getSize().height) / 2);
     System.out.println("gui done");
   }
+ 
 
   @Override
   public void actionPerformed(ActionEvent e) {
@@ -77,8 +143,30 @@ public class Gui extends JPanel implements ActionListener {
       }
     });
     frame.pack();
-    frame.setSize(500, 300);
+    frame.setSize(600, 600);
     frame.setVisible(true);
+  }
+  
+  public void addSlider(JSlider s, String description, String dataParam)
+  {
+	  ChangeListener listener = new ChangeListener()
+	    {
+	       public void stateChanged(ChangeEvent event)
+	       {
+	          // update text field when the slider value changes
+	    	   DataGenerator dataGen = new DataGenerator();
+	    	   
+	    	   
+	          JSlider source = (JSlider) event.getSource();
+	          dataGen.updateParam(dataParam, (double) source.getValue());
+	       }
+	    };
+	    
+	 s.addChangeListener(listener);
+     JPanel panel = new JPanel();
+     panel.add(new JLabel(description));
+     panel.add(s);
+     sliderPanel.add(panel);
   }
   
 }
